623e459b7bfecd3fc84418a2730b8b6b2a04cdca
prayatna <shresthaprayatna1@gmail.com>
2025-08-21 21:40:18 +0545

    File Handling Examples

--- File_handling/program3.cpp
+++ File_handling/program3.cpp
@@ -1,10 +1,30 @@
-#include<iostream>
-#include<fstream>
+#include <iostream>
+#include <fstream>
 using namespace std;
-int main()
-{
-    ofstream file1("test.txt");
-    file1<<"Hello I am Jose Maurinhoo"<<endl;
-    file1.close();
+
+int main() {
+    ifstream inFile("num.txt");   // Input file
+    ofstream oddFile("odd.txt");  // File for odd numbers
+    ofstream evenFile("even.txt"); // File for even numbers
+
+    if (!inFile) {
+        cout << "Error: Could not open num.txt" << endl;
+        return 1;
+    }
+
+    int num;
+    while (inFile >> num) { // Read numbers from file
+        if (num % 2 == 0)
+            evenFile << num << " ";
+        else
+            oddFile << num << " ";
+    }
+
+    cout << "Numbers have been separated into odd.txt and even.txt" << endl;
+
+    inFile.close();
+    oddFile.close();
+    evenFile.close();
+
     return 0;
 }
